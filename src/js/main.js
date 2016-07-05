28318d8e9ea8c52cca0068d562182e417ea4b221
Sergey Miron <sergey.miron@icloud.com>
2016-07-05 15:11:02 +0700

    footer updates

--- src/js/main.js
+++ src/js/main.js
@@ -76,6 +76,15 @@ $(document).ready(function() {
         });
     });
 
+    /** Facebook widget */
+    	(function (d, s, id) {
+    		var js, fjs = d.getElementsByTagName(s)[0];
+    		if (d.getElementById(id)) return;
+    		js = d.createElement(s);
+    		js.id = id;
+    		js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+    		fjs.parentNode.insertBefore(js, fjs);
+    	}(document, 'script', 'facebook-jssdk'));
 });
 
 $.extend(true, $.magnificPopup.defaults, {
