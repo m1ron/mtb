f919524158bb260f6aa964b114cdf84aa2493050
Nataliia <sumovskaya@gmail.com>
2016-07-05 13:12:01 +0700

    Index page

--- src/js/main.js
+++ src/js/main.js
@@ -11,47 +11,87 @@ FastClick.attach(document.body);
 /**
  * On document ready
  */
-$(document).ready(function () {
+$(document).ready(function() {
 
-/* Works carousel */
-	$('.works').each(function() {
-		$('.item', this).each(function(){
-			$('.photos', this).slick({
-			  infinite: true,
-			  slidesToShow: 1,
-			  slidesToScroll: 1,
-				arrows: false,
-				dots: true,
-				customPaging : function(slider, i) {
-        return '<span class="dot"></span>';
-    		}
-			});
-		});
-	});
+    /* Works carousel */
+    $('.works').each(function() {
+        $('.item', this).each(function() {
+            $('.photos', this).slick({
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: true,
+                customPaging: function(slider, i) {
+                    return '<span class="dot"></span>';
+                }
+            });
+        });
+    });
 
-	/* Services images type */
-	$('.services').each(function () {
-		$('.image img', this).each(function () {
-			if ($(this).width() * 0.8 >= $(this).height()) {
-				$(this).addClass('landscape');
-			} else {
-				$(this).addClass('portrait');
-			}
-		});
-	});
+    /* About-us carousel */
+    $('.about-us').each(function() {
+        $('.photos', this).slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true,
+            customPaging: function(slider, i) {
+                return '<span class="dot"></span>';
+            }
+        });
+    });
+
+    /* Features carousel */
+    $('.features').each(function() {
+        $('.list', this).slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true,
+            adaptiveHeight: true,
+            mobileFirst: true,
+            customPaging: function(slider, i) {
+                return '<span class="dot"></span>';
+            },
+            responsive: [{
+                breakpoint: 768,
+                settings: {
+                    unslick: true
+                }
+            }]
+        });
+    });
+
+    /* Services images type */
+    $('.services').each(function() {
+        $('.image img', this).each(function() {
+            if ($(this).width() * 0.8 >= $(this).height()) {
+                $(this).addClass('landscape');
+            } else {
+                $(this).addClass('portrait');
+            }
+        });
+    });
 
 });
 
 $.extend(true, $.magnificPopup.defaults, {
-	tClose: 'Close (Esc)',
-	tLoading: '',
-	closeMarkup: '<div title="%title%" class="mfp-close">&times;</div>',
-	ajax: {tError: '<a href="%url%">Content</a> not found.'},
-	settings: {cache: false},
-	mainClass: 'mfp-zoom-in',
-	midClick: true,
-	removalDelay: 300,
-	autoFocusLast: false,
-	preload: false,
-	fixedContentPos: false
+    tClose: 'Close (Esc)',
+    tLoading: '',
+    closeMarkup: '<div title="%title%" class="mfp-close">&times;</div>',
+    ajax: {
+        tError: '<a href="%url%">Content</a> not found.'
+    },
+    settings: {
+        cache: false
+    },
+    mainClass: 'mfp-zoom-in',
+    midClick: true,
+    removalDelay: 300,
+    autoFocusLast: false,
+    preload: false,
+    fixedContentPos: false
 });
