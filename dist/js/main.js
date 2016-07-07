d0fcc6ba15fbaed5f144550e5967d855d7d5ac28
Sergey Miron <sergey.miron@icloud.com>
2016-07-07 20:15:28 +0700

    desktop header & nav

--- dist/js/main.js
+++ dist/js/main.js
@@ -163,7 +163,6 @@ $(document).ready(function() {
     });
 
     /** Facebook widget */
-    /*
     (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
@@ -172,7 +171,6 @@ $(document).ready(function() {
         js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
-    */
 });
 
 $.extend(true, $.magnificPopup.defaults, {
