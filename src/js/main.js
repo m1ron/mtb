9ebd8acae2d0d647ed4a142899f6fb983022e921
Sergey Miron <sergey.miron@icloud.com>
2016-07-07 16:14:37 +0700

    header & navigation

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,65 @@ FastClick.attach(document.body);
  */
 $(document).ready(function() {
 
+    /** NAVIGATION */
+    $('.nav').each(function() {
+        function toggleNav() {
+            function keyhandler(e) {
+                if (e.which == 27) {
+                    close();
+                }
+            }
+
+            function clickhandler(e) {
+                if (!flag) {
+                    flag = true;
+                    setTimeout(function() {
+                        flag = false;
+                    }, 200);
+                    if (($(e.target).closest('.nav').length <= 0) && ($(e.target).closest('.toggle').length <= 0)) {
+                        close();
+                    }
+                }
+            }
+
+            function open() {
+                html.on('keydown', keyhandler).addClass('nav-visible');
+                body.on('click touchstart', clickhandler);
+                setTimeout(function() {
+                    html.addClass('nav-open');
+                    setTimeout(function() {
+                        html.addClass('nav-done');
+                    }, 400);
+                }, 20);
+            }
+
+            function close() {
+                html.off('keydown', keyhandler).removeClass('nav-open');
+                body.off('click touchstart', clickhandler);
+                setTimeout(function() {
+                    html.removeClass('nav-visible').removeClass('nav-done');
+                }, 420);
+            }
+
+            if (html.hasClass('nav-open')) {
+                close();
+            } else {
+                open();
+            }
+            return false;
+        }
+
+        var body = $('body'),
+            html = $('html'),
+            toggle = $('.header .toggle'),
+            ul = $('ul', this),
+            flag = false;
+        $('<li/>').text('close').wrapInner('<a href="#" class="close"/>').appendTo(ul);
+        $('<i/>').text('x').appendTo(toggle);
+        var close = $('.close', this);
+        toggle.add(close).on('click', toggleNav);
+    });
+
     /* Works carousel */
     $('.works').each(function() {
         $('.item', this).each(function() {
@@ -45,8 +104,9 @@ $(document).ready(function() {
 
     /* Features carousel */
     $('.features').each(function() {
-      var w = $(window), carousel, refresh = false;
-      carousel = $('.list', this).slick({
+        var w = $(window),
+            carousel, refresh = false;
+        carousel = $('.list', this).slick({
             infinite: true,
             slidesToShow: 1,
             slidesToScroll: 1,
@@ -57,30 +117,27 @@ $(document).ready(function() {
             customPaging: function(slider, i) {
                 return '<span class="dot"></span>';
             },
-            responsive: [
-              {
-                    breakpoint: 767,
-                    settings: {
+            responsive: [{
+                breakpoint: 767,
+                settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2,
-                  }
-              },
-              {
-                    breakpoint: 1023,
-                    settings: 'unslick'
-              }
-            ]
+                }
+            }, {
+                breakpoint: 1023,
+                settings: 'unslick'
+            }]
         });
-        w.resize(function () {
-    			if (w.width() >= 1024) {
-    				if (refresh) {
-    					carousel[0].slick.refresh();
-    					refresh = false;
-    				}
-    			} else if (!refresh) {
-    				refresh = true;
-    			}
-    		}).trigger('resize');
+        w.resize(function() {
+            if (w.width() >= 1024) {
+                if (refresh) {
+                    carousel[0].slick.refresh();
+                    refresh = false;
+                }
+            } else if (!refresh) {
+                refresh = true;
+            }
+        }).trigger('resize');
     });
 
     /* Services images type */
@@ -94,15 +151,28 @@ $(document).ready(function() {
         });
     });
 
+    /* Text-block images type */
+    $('.text-block').each(function() {
+        $('.image img', this).each(function() {
+            if ($(this).width() * 0.8 >= $(this).height()) {
+                $(this).addClass('landscape');
+            } else {
+                $(this).addClass('portrait');
+            }
+        });
+    });
+
     /** Facebook widget */
-    	(function (d, s, id) {
-    		var js, fjs = d.getElementsByTagName(s)[0];
-    		if (d.getElementById(id)) return;
-    		js = d.createElement(s);
-    		js.id = id;
-    		js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
-    		fjs.parentNode.insertBefore(js, fjs);
-    	}(document, 'script', 'facebook-jssdk'));
+    /*
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    */
 });
 
 $.extend(true, $.magnificPopup.defaults, {
