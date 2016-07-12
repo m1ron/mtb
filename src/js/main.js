eb23ef257bc0d0ebc9529714c4ae2051add48950
Sergey Miron <sergey.miron@icloud.com>
2016-07-12 18:11:10 +0700

    popup window

--- src/js/main.js
+++ src/js/main.js
@@ -2,16 +2,95 @@
 /*global window, console, document, $, jQuery, google */
 
 
-/**
- * Detect mobile platforms
- */
+/** Remove tap delay on touch devices */
 FastClick.attach(document.body);
 
+/** Magnific Popup Settings */
+$.extend(true, $.magnificPopup.defaults, {
+    closeMarkup: '<span title="%title%" class="mfp-close">x</span>',
+    gallery: {
+        arrowMarkup: '<div title="%title%" class="mfp-arrow mfp-arrow-%dir%"></div>'
+    },
+    settings: {
+        cache: false
+    },
+    mainClass: 'mfp-slide-in',
+    removalDelay: 800,
+    midClick: true,
+    autoFocusLast: false,
+    preload: false,
+    fixedContentPos: true,
+    fixedBgPos: true
+});
 
-/**
- * On document ready
- */
+/** On document ready */
 $(document).ready(function() {
+
+    /** Sticky header */
+    (function() {
+        function stick() {
+            html.addClass('nav-sticky');
+            setTimeout(function() {
+                html.addClass('nav-sticky-animate');
+                sticked = true;
+            }, 20);
+        }
+
+        function unstick() {
+            html.removeClass('nav-sticky-animate');
+            setTimeout(function() {
+                html.removeClass('nav-sticky');
+                sticked = false;
+            }, 200);
+        }
+
+        function check() {
+            if (w.scrollTop() > minHeight) {
+                if (!sticked) {
+                    stick();
+                }
+            } else {
+                if (sticked) {
+                    unstick();
+                }
+            }
+        }
+
+        function init() {
+            inited = true;
+            check();
+            w.on('scroll touchmove', check);
+        }
+
+        function uninit() {
+
+            inited = false;
+            html.removeClass('nav-sticky');
+            w.off('scroll touchmove', check);
+        }
+
+        function onResize() {
+            if (w.width() > minWidth) {
+                if (!inited) {
+                    init();
+                }
+            } else {
+                if (inited) {
+                    uninit();
+                }
+            }
+        }
+
+        var html = $('html'),
+            w = $(window),
+            sticked = false,
+            inited = false,
+            minWidth = 1170,
+            minHeight = 300;
+
+        w.on('resize', onResize);
+        onResize();
+    }());
 
     /** Navigation */
     $('.nav').each(function() {
@@ -168,76 +247,8 @@ $(document).ready(function() {
         }
     });
 
-    /** Sticky header */
-    (function() {
-        function stick() {
-            console.log("stick");
-            html.addClass('nav-sticky');
-            setTimeout(function() {
-                html.addClass('nav-sticky-animate');
-                sticked = true;
-            }, 20);
-        }
-
-        function unstick() {
-            console.log("unstick");
-            html.removeClass('nav-sticky-animate');
-            setTimeout(function() {
-                html.removeClass('nav-sticky');
-                sticked = false;
-            }, 100);
-        }
-
-        function check() {
-            if (w.scrollTop() > minHeight) {
-                if (!sticked) {
-                    stick();
-                }
-            } else {
-                if (sticked) {
-                    unstick();
-                }
-            }
-        }
-
-        function init() {
-            console.log('init');
-            inited = true;
-            check();
-            w.on('scroll touchmove', check);
-        }
-
-        function uninit() {
-            console.log('uninit');
-            inited = false;
-            html.removeClass('nav-sticky');
-            w.off('scroll touchmove', check);
-        }
-
-        function onResize() {
-            console.log('onResize');
-            if (w.width() > minWidth) {
-                if (!inited) {
-                    init();
-                }
-            } else {
-                if (inited) {
-                    uninit();
-                }
-            }
-        }
-
-        var html = $('html'),
-            w = $(window),
-            sticked = false,
-            inited = false,
-            minWidth = 1170,
-            minHeight = 250;
-
-        console.log('sticky');
-        w.on('resize', onResize);
-        onResize();
-    }());
+    /** Popup images */
+    $('.open-popup').magnificPopup();
 
     /** Facebook widget */
     (function(d, s, id) {
@@ -248,22 +259,4 @@ $(document).ready(function() {
         js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
-});
-
-$.extend(true, $.magnificPopup.defaults, {
-    tClose: 'Close (Esc)',
-    tLoading: '',
-    closeMarkup: '<div title="%title%" class="mfp-close">&times;</div>',
-    ajax: {
-        tError: '<a href="%url%">Content</a> not found.'
-    },
-    settings: {
-        cache: false
-    },
-    mainClass: 'mfp-zoom-in',
-    midClick: true,
-    removalDelay: 300,
-    autoFocusLast: false,
-    preload: false,
-    fixedContentPos: false
 });
